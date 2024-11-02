01ce2a92386d070f2363cda532fd28b81f3069da
shivraj <209shivrajsingh@gmail.com>
2024-11-02 18:34:24 +0530

    patterns

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5] = {10, 20, 30, 40, 50};  // Array of 5 integers
+
+    printf("Array elements are:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Element %d: %d\n", i + 1, numbers[i]);
+    }
+
+    return 0;
+}
