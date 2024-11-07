76a6ab6dd7247ec8279230e25bd9e4b1bc1f825c
shivraj <209shivrajsingh@gmail.com>
2024-11-07 17:57:51 +0530

    pattern

---
+++ patterns/pattern9.c
@@ -0,0 +1,14 @@
+/*pattern 9*/
+#include <stdio.h>
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) printf("*");
+        printf("\n");
+    }
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 1; j <= i; j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
