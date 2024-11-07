d0183db787fa8322bab69b8702aa21edf9ffdac6
shivraj <209shivrajsingh@gmail.com>
2024-11-07 17:55:55 +0530

    pattern

--- patterns/pattern8.c
+++ patterns/pattern8.c
@@ -1,1 +1,12 @@
 /*pattern 8*/
+#include <stdio.h>
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", (i + j) % 2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
