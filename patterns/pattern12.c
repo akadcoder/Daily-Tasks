2486b3e857e5de2be58f32ad017e1bd0bf8d6428
shivraj <209shivrajsingh@gmail.com>
2024-11-07 17:58:43 +0530

    pattern

---
+++ patterns/pattern12.c
@@ -0,0 +1,12 @@
+/*pattern 12*/
+#include <stdio.h>
+int main() {
+    int n = 5, count = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", count++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
