cabd8a1b304a8c8c3c7030fef16a07cfa4e1ea85
Shivraj Singh <209shivrajsingh@gmail.com>
2024-11-23 20:52:26 +0530

    Update array.c

--- array.c
+++ array.c
@@ -2,12 +2,9 @@
 
 int main() {
     int numbers[5] = {10, 20, 30, 40, 50};  // Array of 5 integers
-
     printf("Array elements are:\n");
-    for (int i = 0; i < 5; i++) {
-        
+    for (int i = 0; i < 5; i++) {  
         printf("Element %d: %d\n", i + 1, numbers[i]);
     }
-
     return 0;
 }
